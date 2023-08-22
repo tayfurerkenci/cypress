dcd97b62077c3e1e33ccbe8b8c8be6e0da1852d8
Tayfur Erkenci <terkenci@gmail.com>
2023-08-22 22:30:00 +0300

    feat: added then method and unit test like expect method

--- 2-diving-deeper/cypress/e2e/contact.cy.js
+++ 2-diving-deeper/cypress/e2e/contact.cy.js
@@ -6,12 +6,22 @@ describe('contact form', () => {
     cy.get('[data-cy="contact-input-message"]').type('Hello World');
     cy.get('[data-cy="contact-input-name"]').type('John Doe');
     cy.get('[data-cy="contact-input-email"]').type('test@example.com');
+
+    // Alias
     cy.get('[data-cy="contact-btn-submit"]').as('submitBtn');
     cy.get('@submitBtn')
       .contains('Send Message')
       .and('not.have.attr', 'disabled');
     cy.get('@submitBtn').click();
     cy.get('@submitBtn').should('have.attr', 'disabled');
+
+    // Direct Access to the Element
+    // el is the subject of get()
+    cy.get('[data-cy="contact-btn-submit"]').then((el) => {
+      // This lines are like jest or vite unit tests !
+      expect(el.attr('disabled')).to.be.undefined;
+      expect(el.text()).to.equal('Send Message');
+    });
 
     // Not Recommended Way
     // const submitBtn = cy.get('[data-cy="contact-btn-submit"]');
