6c82bf4b2d68d66688d5cd2274812187beefda09
Tayfur Erkenci <terkenci@gmail.com>
2023-08-27 21:16:05 +0300

    feat: implemented interceptor

--- 4-network-databases-auth/cypress/e2e/newsletter.cy.js
+++ 4-network-databases-auth/cypress/e2e/newsletter.cy.js
@@ -4,9 +4,33 @@ describe('Newsletter', () => {
   });
 
   it('should display a success message', () => {
+    // block the POST request to the newsletter endpoint, and return status 201
+    cy.intercept('POST', '/newsletter*', {
+      status: 201,
+    }).as('subscribe');
+
     cy.visit('/');
     cy.get('[data-cy="newsletter-email"]').type('tayfur@example.com');
     cy.get('[data-cy="newsletter-submit"]').click();
+
+    // wait the interceptor block the request
+    cy.wait('@subscribe');
     cy.contains('Thanks for signing up!');
+  });
+
+  it('should display validation errors', () => {
+    // block the POST request to the newsletter endpoint, and return validaton errors
+    const validationError = 'Email exists already.';
+    cy.intercept('POST', '/newsletter*', {
+      message: validationError,
+    }).as('subscribe');
+
+    cy.visit('/');
+    cy.get('[data-cy="newsletter-email"]').type('tayfur@example.com');
+    cy.get('[data-cy="newsletter-submit"]').click();
+
+    // wait the interceptor block the request
+    cy.wait('@subscribe');
+    cy.contains(validationError);
   });
 });
